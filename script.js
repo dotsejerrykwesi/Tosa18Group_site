8b8caf7b174825b69adff9bbd2722d182fc172f7
school-project <139367181+dotsejerrykwesi@users.noreply.github.com>
2026-04-19 16:47:21 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// SCROLL ANIMATION
+const elements = document.querySelectorAll(".animate");
+
+window.addEventListener("scroll", () => {
+  elements.forEach(el => {
+    const position = el.getBoundingClientRect().top;
+    const screenHeight = window.innerHeight;
+
+    if (position < screenHeight - 100) {
+      el.classList.add("show");
+    }
+  });
+});
